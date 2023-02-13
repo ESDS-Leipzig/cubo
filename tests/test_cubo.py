3c8d438cf9560ada42f48f64028866ce68978851
davemlz <dml.mont@gmail.com>
2023-02-13 22:47:59 +0100

    TESTS: Added tests for Planetary Computer and Element84 STACs

--- tests/test_cubo.py
+++ tests/test_cubo.py
@@ -10,9 +10,34 @@ import cubo
 class Test(unittest.TestCase):
     """Tests for the cubo package."""
 
-    def test_0(self):
+    def test_planetary_computer(self):
         """Test the cubo"""
-        self.assertIsInstance(1, int)
+        da = cubo.create(
+            lat=50,
+            lon=10,
+            collection="sentinel-2-l2a",
+            bands=["B02","B03","B04"],
+            start_date="2021-06-01",
+            end_date="2021-06-10",
+            edge_size=32,
+            resolution=10,
+        )
+        self.assertIsInstance(da, xr.DataArray)
+
+    def test_element84(self):
+        """Test the cubo"""
+        da = cubo.create(
+            lat=50,
+            lon=10,
+            collection="sentinel-s2-l2a-cogs",
+            bands=["B02","B03","B04"],
+            start_date="2021-06-01",
+            end_date="2021-06-10",
+            edge_size=32,
+            resolution=10,
+            stac="https://earth-search.aws.element84.com/v0"
+        )
+        self.assertIsInstance(da, xr.DataArray)
 
 
 if __name__ == "__main__":
